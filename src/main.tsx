db76a3221dd3790608bb5c5f8d45905b3b695f6c
inaralmeida <inara.ralmeida@gmail.com>
2023-10-19 14:07:58 -0300

    style: settings theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import Routes from './Routes.tsx'
+import { theme } from './style/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Routes />
+		<ThemeProvider theme={theme}>
+			<Routes />
+		</ThemeProvider>
 	</React.StrictMode>
 )
