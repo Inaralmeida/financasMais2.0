68d6805148dba915b7837e294cc56b7beefe3292
inaralmeida <inara.ralmeida@gmail.com>
2023-10-21 22:40:24 -0300

    feat: add images

---
+++ src/assets/images/images.ts
@@ -0,0 +1,19 @@
+import banner1 from './home/LP-banner1.svg'
+import banner2 from './home/LP-banner2.svg'
+import banner3 from './home/LP-banner3.svg'
+import lpicon1 from './home/LP-icon1.svg'
+import lpicon2 from './home/LP-icon2.svg'
+import lpicon3 from './home/LP-icon3.svg'
+import lpprofile from './home/profile.svg'
+
+export default {
+	home: {
+		banner1,
+		banner2,
+		banner3,
+		lpicon1,
+		lpicon2,
+		lpicon3,
+		lpprofile
+	}
+}
