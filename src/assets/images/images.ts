67a3bdda61ca4c54685306ddbb51abcaf190a897
inaralmeida <inara.ralmeida@gmail.com>
2023-10-22 22:16:54 -0300

    fix: move imgs for public

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,14 +1,14 @@
-import banner1 from './home/LP-banner1.svg'
-import banner2 from './home/LP-banner2.svg'
-import banner3 from './home/LP-banner3.svg'
-import lpicon1 from './home/LP-icon1.svg'
-import lpicon2 from './home/LP-icon2.svg'
-import lpicon3 from './home/LP-icon3.svg'
-import bgbanner1 from './home/bg-banner1.svg'
-import lpprofile from './home/profile.svg'
 import IconClose from './icons/IconClose'
 import IconOpenMenu from './icons/IconOpen'
 import IconPlus from './icons/IconPlus'
+import banner1 from '/home/LP-banner1.svg'
+import banner2 from '/home/LP-banner2.svg'
+import bgbanner1 from '/home/bg-banner1.svg'
+import banner3 from '/home/lp-banner3.svg'
+import lpicon1 from '/home/lp-icon1.svg'
+import lpicon2 from '/home/lp-icon2.svg'
+import lpicon3 from '/home/lp-icon3.svg'
+import lpprofile from '/home/profile.svg'
 export default {
 	home: {
 		banner1,
